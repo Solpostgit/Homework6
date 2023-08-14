8da1eec5c182dfda69352d94b0a16cb61c563e06
Solpostgit <begemot_off@mail.ru>
2023-08-14 21:18:24 +0300

    Added task10

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         task6();
         task7();
         task8();
-        task9();/*
-        task10();*/
+        task9();
+        task10();
     }
 
     public static void task1() {
@@ -88,6 +88,16 @@ public class Main {
             total = total + total / 100;
             total = total + bankDeposit;
             System.out.printf("Месяц %d, сумма накоплений равна %.2f рублей \n", i, total);
+        }
+    }
+
+    public static void task10() {
+        System.out.println();
+        System.out.println("Task 10");
+        int productNumbers = 0;
+        for (int i = 1; i <= 10; i++) {
+            productNumbers = 2 * i;
+            System.out.printf("2*%d=%d \n", i, productNumbers);
         }
     }
 
