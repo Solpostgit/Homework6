48e7ac6aadf266175a5a066412d7eae39bf16e58
Solpostgit <begemot_off@mail.ru>
2023-08-14 19:44:01 +0300

    Added task1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();/*
-        task2();
+        task2();/*
         task3();
         task4();
         task5();
@@ -14,7 +14,10 @@ public class Main {
 
     public static void task1() {
         System.out.println("Task 1");
-
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println();
     }
 
 }
